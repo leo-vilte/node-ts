d12f65421f4c744613930717687f21692fcfdbba
leo.vilte <vilte@decrypto.la>
2023-11-01 15:18:51 -0300

    cambios

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,46 @@
-console.log(process.env.PORT)
+
+import axios from 'axios';
+
+
+var xml2js = require('xml2js');
+
+console.log(process.env.PORT);
+console.log("fsds2222");
+
+
+  const axiosConfig = {
+    headers: {
+      Accept: 'application/xml',
+      'Content-Type': 'text/xml;charset=UTF-8',
+    },
+  };
+  export const getDecrypto = (): any => {
+    const url = 'https://api.decrypto.la/1.0/derivatives/prices';
+    return  axios.get(url, axiosConfig).then(async (response) => {
+      return await new Promise((resolve, reject) => {
+        const parser = new xml2js.Parser();
+        parser.parseString(response.data, (err, result) => {
+          if (err) {
+            reject(err);
+          } else {
+            const prices = result.GetPriceBalanceResponseDto;
+            resolve({
+              compra: prices.highestLongPrice[0],
+              venta: prices.lowestShortPrice[0],
+            });
+          }
+        });
+      });
+    });
+  };
+
+
+
+
+  getDecrypto().then(
+    async data => {
+        console.log(data);
+      }
+  )
+
+  console.log("end");
